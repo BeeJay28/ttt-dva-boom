574142f7142815b8c2786c032aee5a552edc5c45
Artur Herdt <herdt.artur28@yahoo.com>
2023-08-20 14:34:50 +0200

    Remove unneeded milkgun from convars and entity file

--- lua/autorun/sh_milkgun_convars.lua
+++ lua/autorun/sh_milkgun_convars.lua
@@ -1,38 +1,26 @@
 if engine.ActiveGamemode() ~= "terrortown" then return end
 
 -- convars added with default values
-CreateConVar("ttt_milkgun_primary_sound", "1", {FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Sound of the primary attack")
-
-CreateConVar("ttt_milkgun_secondary_sound", "1", {FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Sound of the secondary attack")
-
-CreateConVar("ttt_milkgun_automaticFire", "0", {FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Enable automatic fire")
+CreateConVar("ttt_milkgun_secondary_sound", "1", {FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Sound of the taunt secondary attack")
 
 CreateConVar("ttt_milkgun_damage", "150", {FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Damage dealt on impact")
 
 CreateConVar("ttt_milkgun_randomDamage", "5", {FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Applied on top of the normal damage (+/-)")
 
-CreateConVar("ttt_milkgun_ammo", "3", {FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Default ammo the milkgun has when bought")
-
-CreateConVar("ttt_milkgun_clipSize", "3", {FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Clipsize of the milkgun")
-
-CreateConVar("ttt_milkgun_rps", "0.4", {FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Packages of milk to shoot per second")
+CreateConVar("ttt_milkgun_edmMode", "1", {FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Enable EDM mode")
 
 hook.Add("TTTUlxInitCustomCVar", "TTTMilkGunInitRWCVar", function(name)
-    ULib.replicatedWritableCvar("ttt_milkgun_primary_sound", "rep_ttt_milkgun_primary_sound", GetConVar("ttt_milkgun_primary_sound"):GetBool(), true, false, name)
     ULib.replicatedWritableCvar("ttt_milkgun_secondary_sound", "rep_ttt_milkgun_secondary_sound", GetConVar("ttt_milkgun_secondary_sound"):GetBool(), true, false, name)
-    ULib.replicatedWritableCvar("ttt_milkgun_automaticFire", "rep_ttt_milkgun_automaticFire", GetConVar("ttt_milkgun_automaticFire"):GetBool(), true, false, name)
     ULib.replicatedWritableCvar("ttt_milkgun_damage", "rep_ttt_milkgun_damage", GetConVar("ttt_milkgun_damage"):GetInt(), true, false, name)
     ULib.replicatedWritableCvar("ttt_milkgun_randomDamage", "rep_ttt_milkgun_randomDamage", GetConVar("ttt_milkgun_randomDamage"):GetInt(), true, false, name)
-    ULib.replicatedWritableCvar("ttt_milkgun_ammo", "rep_ttt_milkgun_ammo", GetConVar("ttt_milkgun_ammo"):GetInt(), true, false, name)
-    ULib.replicatedWritableCvar("ttt_milkgun_clipSize", "rep_ttt_milkgun_clipSize", GetConVar("ttt_milkgun_clipSize"):GetInt(), true, false, name)
-    ULib.replicatedWritableCvar("ttt_milkgun_rps", "rep_ttt_milkgun_rps", GetConVar("ttt_milkgun_rps"):GetFloat(), true, false, name)
+    ULib.replicatedWritableCvar("ttt_milkgun_edmMode", "rep_ttt_milkgun_edmMode", GetConVar("ttt_milkgun_edmMode"):GetBool(), true, false, name)
 end)
 
 if CLIENT then
     -- Use string or string.format("%.f",<steamid64>) 
     -- addon dev emblem in scoreboard
     hook.Add("TTT2FinishedLoading", "TTT2RegistermexikoediAddonDev", function()
-        AddTTT2AddonDev("76561198279816989")
+        -- AddTTT2AddonDev("76561198279816989")
     end)
 
     hook.Add("TTTUlxModifyAddonSettings", "TTTMilkGunModifySettings", function(name)
@@ -53,20 +41,8 @@ if CLIENT then
         tttrslst1:SetSpacing(5)
 
         tttrslst1:AddItem(xlib.makecheckbox{
-            label = "ttt_milkgun_primary_sound (Def. 1)",
-            repconvar = "rep_ttt_milkgun_primary_sound",
-            parent = tttrslst1
-        })
-
-        tttrslst1:AddItem(xlib.makecheckbox{
             label = "ttt_milkgun_secondary_sound (Def. 1)",
             repconvar = "rep_ttt_milkgun_secondary_sound",
-            parent = tttrslst1
-        })
-
-        tttrslst1:AddItem(xlib.makecheckbox{
-            label = "ttt_milkgun_automaticFire (Def. 0)",
-            repconvar = "rep_ttt_milkgun_automaticFire",
             parent = tttrslst1
         })
 
@@ -85,33 +61,6 @@ if CLIENT then
             min = 0,
             max = 50,
             decimal = 0,
-            parent = tttrslst1
-        })
-
-        tttrslst1:AddItem(xlib.makeslider{
-            label = "ttt_milkgun_ammo (Def. 3)",
-            repconvar = "rep_ttt_milkgun_ammo",
-            min = 0,
-            max = 10,
-            decimal = 0,
-            parent = tttrslst1
-        })
-
-        tttrslst1:AddItem(xlib.makeslider{
-            label = "ttt_milkgun_clipSize (Def. 3)",
-            repconvar = "rep_ttt_milkgun_clipSize",
-            min = 0,
-            max = 10,
-            decimal = 0,
-            parent = tttrslst1
-        })
-
-        tttrslst1:AddItem(xlib.makeslider{
-            label = "ttt_milkgun_rps (Def. 0.4)",
-            repconvar = "rep_ttt_milkgun_rps",
-            min = 0,
-            max = 10,
-            decimal = 1,
             parent = tttrslst1
         })
 
--- lua/entities/ent_ttt_ttt2_milk_gun.lua
+++ lua/entities/ent_ttt_ttt2_milk_gun.lua
@@ -9,10 +9,6 @@ ENT.Purpose = "Milk entity for the milk gun."
 ENT.Spawnable = false
 ENT.AdminSpawnable = false
 ENT.AdminOnly = false
-ENT.AlreadyHit = {}
-ENT.Collided = 0
-local CollisionsBeforeRemove = 20
-local MinSpeed = 700
 
 if SERVER then
     AddCSLuaFile()
@@ -24,51 +20,12 @@ if SERVER then
         self:SetSolid(SOLID_VPHYSICS)
         local phys = self:GetPhysicsObject()
 
-        if phys:IsValid() then
+        if phys:IsValid() then -- Why?
             phys:Wake()
-            phys:SetMass(5)
         end
 
-        self:GetPhysicsObject():SetMass(2)
+        phys:SetMass(500)
         self:SetUseType(SIMPLE_USE)
-    end
-
-    function ENT:PhysicsCollide(data, phys)
-        self.Collided = self.Collided + 1
-
-        if self.Collided <= CollisionsBeforeRemove then
-            local Ent = data.HitEntity
-            if not IsValid(self) or not IsValid(Ent) or not Ent:IsPlayer() then return end
-
-            if not self.AlreadyHit[Ent:GetName()] and self:GetVelocity():LengthSqr() > MinSpeed * MinSpeed then
-                local dmg = DamageInfo()
-
-                if IsValid(self:GetOwner()) then
-                    dmg:SetAttacker(self:GetOwner())
-                end
-
-                local inflictor = ents.Create("weapon_ttt_ttt2_milk_gun")
-                dmg:SetInflictor(inflictor)
-                local r = GetConVar("ttt_milkgun_randomDamage"):GetFloat()
-                local rand = math.random(-r, r)
-                local dm = GetConVar("ttt_milkgun_damage"):GetInt() + rand
-                dmg:SetDamage(dm > 0 and dm or 0)
-                dmg:SetDamageType(DMG_GENERIC)
-                Ent:TakeDamageInfo(dmg)
-                local effectdata = EffectData()
-                effectdata:SetStart(data.HitPos)
-                effectdata:SetOrigin(data.HitPos)
-                effectdata:SetScale(1)
-                util.Effect("BloodImpact", effectdata)
-                self.AlreadyHit[Ent:GetName()] = true
-            end
-        else
-            timer.Simple(0, function()
-                if IsValid(self) then
-                    self:Remove()
-                end
-            end)
-        end
     end
 end
 
